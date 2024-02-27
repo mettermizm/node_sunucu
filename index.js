42340f6c41e16b4336ceb2e6cf809e01b9839eff
Mehmet Fidan <85038013+mettermizm@users.noreply.github.com>
2024-02-27 23:25:19 +0300

    nıde

--- index.js
+++ index.js
@@ -1,26 +1,26 @@
-const express = require('express');
-const http = require('http');
-const WebSocket = require('ws');
+const puppeteer = require('puppeteer');
 
-const app = express();
-const server = http.createServer(app);
-// IP adresinizi burada belirtin
-const wss = new WebSocket.Server({ server, host: '192.168.0.119', port: 4000 });
+(async () => {
+  // Tarayıcıyı başlat ve boş bir sayfa aç
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
 
-app.get('/', (req, res) => {
-    res.send('Merhaba, Express.js ile basit bir web sitesine hoş geldiniz!');
-});
+  // WhatsApp Web'e git
+  await page.goto('https://web.whatsapp.com');
 
-wss.on('connection', (ws) => {
-    console.log('Yeni bir WebSocket bağlantısı kuruldu.');
+  // Sohbet listesinden belirli bir sohbete gidin (örneğin, bir kişi veya grup sohbeti)
+  // Not: Bu adımı manuel olarak gerçekleştirmeniz gerekebilir
+  // await page.click('belirli bir sohbetin CSS seçicisi'); // Burada belirli bir sohbetin CSS seçicisini eklemeniz gerekecek
 
-    ws.on('message', (message) => {
-        console.log('Alınan mesaj: %s', message);
-        ws.send(`Gelen mesajı aldım: ${message}`);
-    });
-});
+  // Mesaj gönderme alanını bulun ve mesajı gönderin
+  const messageInputSelector = '.selectable-text';
+  await page.waitForSelector(messageInputSelector);
+  await page.type(messageInputSelector, 'Merhaba, bu bir test mesajıdır. Mehmet Fidan bu mesajı yazılım ile göndermiştir ile gönderilmiştir.');
+  await page.keyboard.press('Enter');
 
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-    console.log(`Sunucu ${PORT} numaralı portta çalışıyor.`);
-});
+  await browser.close();
+})();
+
+
+
+//https://media.fadb6-1.fna.whatsapp.net/mms/image/tLb9zjZ60fQchK5sgncsoQP2YlVxlH2R-8MAKMy3dBs=?token=tLb9zjZ60fQchK5sgncsoQP2YlVxlH2R-8MAKMy3dBs%3D&d_md=L3YvdDYyLjMxMTExLTI0LzM3ODA4ODc0OV85MTczNTQ5ODk3OTc2NTlfNzI4OTU3NjE0ODcxMTk5NTgxX24uZW5j&auth=Ac30fARM-H8I0Ai6VZlvjh0bTKp1PwRej-2URrA9J7z2Xg
