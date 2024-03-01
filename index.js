43b5c6b5b7fe644d1b34d134b3bb2671c263fc8d
Mehmet Fidan <85038013+mettermizm@users.noreply.github.com>
2024-03-01 13:29:46 +0300

    istek

--- index.js
+++ index.js
@@ -7,6 +7,16 @@ app.get('/about', (req, res) => {
   res.send('Hakkında sayfası');
 });
 
+const json = {
+  "baslik" : "Hoşgeldiniz",
+  "content" : "Bugün nasılsınız"
+}
+
+// Hakkında sayfası
+app.get('/istek', (req, res) => {
+  res.send(json);
+});
+
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
 
