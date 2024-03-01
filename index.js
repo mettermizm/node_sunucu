91b3c714a594370c48cae9449db3170f89f2db9e
Mehmet Fidan <85038013+mettermizm@users.noreply.github.com>
2024-03-01 12:21:32 +0300

    abouyt

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const express = require("express"); 
 const app = express(); 
 app.get("/", (req, res) => { res.send("Express on Vercel"); }); 
+
+// Hakkında sayfası
+app.get('/about', (req, res) => {
+  res.send('Hakkında sayfası');
+});
+
 const PORT = process.env.PORT || 5000; 
 app.listen(PORT, () => { console.log(`Server is running on port ${PORT}`); });
+
+
 
 
 
